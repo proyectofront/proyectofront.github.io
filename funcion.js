19300e23f367e923e2eb13ea2873f307a9ea2e4d
proyectofront <126578833+proyectofront@users.noreply.github.com>
2023-03-20 12:58:46 -0300

    initial commit

---
+++ funcion.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton1').onclick = function () {
+    document.getElementById("demo").innerHTML = "2014 - Curso 'APORTE DEL TRANSPORTE AL COMERCIO EXTERIOR' - Universidad Austral - Rosario, Santa Fe 2020";
+
+}
+
